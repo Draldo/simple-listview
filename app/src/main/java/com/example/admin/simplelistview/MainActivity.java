57fb018ba710a3239065b00f6eda99173a155b03
Draldo <draldo_lopez@hotmail.com>
2016-08-01 17:28:02 -0400

    list with icon

--- app/src/main/java/com/example/admin/simplelistview/MainActivity.java
+++ app/src/main/java/com/example/admin/simplelistview/MainActivity.java
@@ -2,12 +2,50 @@ package com.example.admin.simplelistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private EditText mEditText;
+    private String[] names;
+    private List<String> data;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.a_main_list);
+
+        names  = new String[]{"Chris", "Aldo", "Mike", "Libu", "Karles","Pedro","Bart","Bort","Kyle",
+                "El Brayan", "La Britanny", "El wero", "Dona Pelos","Pirrrurris","Fat joe"};
+
+        data = new ArrayList<String>(Arrays.asList(names));
+
+        //arrayAdapter = new ArrayAdapter<String>(this,android.R.layout_textview_item1, data);
+        arrayAdapter = new ArrayAdapter<String>(this,R.layout.list_item,R.id.a_textview_item, data);
+
+        mListView.setAdapter(arrayAdapter);
+
+    }
+
+    public void addItem(View view) {
+
+        mEditText = (EditText) findViewById(R.id.a_main_et);
+        String text = mEditText.getText().toString();
+        data.add(text);
+        arrayAdapter.notifyDataSetChanged();
+        Toast.makeText(view.getContext(),"Item added succesfully",Toast.LENGTH_SHORT).show();
+        mEditText.setText("");
     }
 }
